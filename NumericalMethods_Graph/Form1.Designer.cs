bc9640b0142df16722558ef50415aa289e8307d5
Настя Тимофеева <ntim14@yandex.ru>
2022-10-11 20:12:39 +0300

    Добавьте файлы проекта.

---
+++ NumericalMethods_Graph/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace NumericalMethods_Graph
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.pictureButton = new System.Windows.Forms.Button();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureButton
+            // 
+            this.pictureButton.Location = new System.Drawing.Point(208, 552);
+            this.pictureButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureButton.Name = "pictureButton";
+            this.pictureButton.Size = new System.Drawing.Size(188, 48);
+            this.pictureButton.TabIndex = 1;
+            this.pictureButton.Text = "Нарисовать";
+            this.pictureButton.UseVisualStyleBackColor = true;
+            this.pictureButton.Click += new System.EventHandler(this.pictureButton_Click);
+            // 
+            // chart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart.Legends.Add(legend2);
+            this.chart.Location = new System.Drawing.Point(9, 10);
+            this.chart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart.Name = "chart";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(582, 293);
+            this.chart.TabIndex = 2;
+            this.chart.Text = "chart1";
+            title3.Name = "Title1";
+            title3.Text = "График функции";
+            this.chart.Titles.Add(title3);
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            this.chart2.Location = new System.Drawing.Point(9, 308);
+            this.chart2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart2.Name = "chart2";
+            series4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.LabelForeColor = System.Drawing.Color.RosyBrown;
+            series4.Name = "Series1";
+            series4.ShadowColor = System.Drawing.Color.DarkSlateBlue;
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(582, 239);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart1";
+            title4.Name = "Title1";
+            title4.Text = "График погрешностей";
+            this.chart2.Titles.Add(title4);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 602);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.pictureButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "График";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button pictureButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ NumericalMethods_Graph/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumericalMethods_Graph
+{
+    public partial class MainForm : Form
+    {
+        public static int m = 10;
+        public static double[] xValues = new double[m + 1];
+        public static double[] fValues = new double[m + 1];
+        public static List<double> errors = new List<double>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureButton_Click(object sender, EventArgs e)
+        {
+            NumericalMethods();
+
+            this.chart.Series[0].Points.Clear();
+
+            for (int i = 0; i < xValues.Length; i++)
+            {
+                this.chart.Series[0].Points.AddXY(xValues[i], fValues[i]);
+            }
+
+            for (int i = 1; i < m; i++)
+            {
+                this.chart2.Series[0].Points.AddXY(xValues[i], errors[i - 1]);
+            
+            }
+            
+        }
+
+        static void NumericalMethods()
+        {
+            double x, q, An, sum;
+            int n;
+            int a = 0, b = 4, N = 5;
+            double h = (double)(b - a) / N;
+
+            for (int i = 0; i < m; i++)
+            {
+                xValues[i] = i * h;
+            }
+
+            int index = 0;
+            for (x = a; x <= b; x += h)
+            {
+                fValues[index] = F(x);
+                index++;
+            }
+
+            h = (double)(b - a) / m;
+            float max = 0;
+           
+            Console.WriteLine("x\t\tf(x)\t\tLnX\t\terr");
+            
+            for (x = a; x <= b; x += h)
+            {
+                float lagrange = (float)Lagrange(xValues, fValues, 6, x);
+                float fx = (float)F(x);
+                float error = Math.Abs(lagrange - fx);
+                errors.Add(error);
+
+                if (error > max)
+                    max = error;
+
+                Console.WriteLine(Math.Round(x, 1) + "\t\t" + fx + "\t" +
+                lagrange + "\t" + error);
+            }
+            Console.WriteLine("max|err| = " + max);
+
+            Console.ReadLine();
+
+
+        }
+
+        public static double F(double x)// считаем F(x)
+        {
+            int n = 0;
+            double sum = 0, q;
+            double An = x;
+            double eps = Math.Pow(10, -6);
+
+            while (Math.Abs(An) >= eps)
+            {
+                sum += An;
+
+                q = ((-1) * x * x * (2 * n + 1)) / ((2 * n + 3) * (2 * n + 3) * (2 * n + 2));
+
+                An *= q;
+
+                n++;
+            }
+
+            return sum;
+        }
+
+        public static double Lagrange(double[] xValues, double[] fValues, int n, double argX)
+        {
+            double con, LnX = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                con = 1;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                        con *= (argX - xValues[j]) / (xValues[i] - xValues[j]);
+                }
+
+                LnX += con * fValues[i];
+            }
+
+            return LnX;
+        }
+    }
+}
